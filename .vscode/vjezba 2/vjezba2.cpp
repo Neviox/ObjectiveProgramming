6e5c1403e3f2ca8fa0e337c6de50b7aae07de028
Tin Martinic <neviox@users.noreply.github.com>
2021-03-11 20:04:53 +0100

    .

--- .vscode/vjezba 2/vjezba2.cpp
+++ .vscode/vjezba 2/vjezba2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
+
 struct Pravokutnik
 {
     int a;
@@ -13,19 +13,19 @@ struct Kruznica
 };
 
 
-int* alocirani_niz(){
+int& alocirani_niz(int &referenca){
     int* niz=(int*)malloc(sizeof(int)*10);
     while(true){
-        int c;
         int x;
         int i=0;
         cin >> x;
-        if(c%10==0){
+        if(referenca%10==0){
             int* niz=(int*)malloc(sizeof(int)*2);
         }
         if(x>0){
             niz[i]=x;
             i++;
+            referenca++;
         }
         else
             break;
@@ -33,7 +33,7 @@ int* alocirani_niz(){
     }
     return niz;
 }
-int* zbroj_prethodnika(int niz[],int n){
+int* zbroj_prethodnika(int *niz,int n){
     int* novi=(int*)malloc(sizeof(int)*n);
     for(int i=0;i<n;i++){
         if(i>1){
@@ -45,37 +45,44 @@ int* zbroj_prethodnika(int niz[],int n){
     }
 return novi;
 }
-int min_max(int niz[],int n,int& min,int* max){
+int min_max(int niz[],int n,int& min,int &max){
     min=niz[0];
-    *max=niz[0];
+    max=niz[0];
     for(int i=0;i<n;i++){
         if(niz[i]<min){
             min=niz[i];
         }
-        if(niz[i]>*max){
-            *max=niz[i];
+        if(niz[i]>max){
+            max=niz[i];
         }
     }
 }
 
 int main(){
-   /* int niz[10]={1,1,3,4,5,6,7,8,9,22};
+    
+    //prvi zadatak!
+    int niz[10]={1,1,3,4,5,6,7,8,9,22};
     int n=10;
     int min;
     int max;
-    min_max(niz,n,min,&max);
+    min_max(niz,n,min,max);
     cout<<"Najmanji je:"<<min<<"\n Najveci je:"<<max<<endl;
+
+    //cetvrti zadatak!
     int* nizbrdo=zbroj_prethodnika(niz,n);
     for(int i=0;i<n;i++){
-        cout<<"Novi niz je "<<nizbrdo[i]<<endl;1
-
+        cout<<"Novi niz je "<<nizbrdo[i]<<endl;
     }
-    free(nizbrdo);*/
-    int* nizakpod=alocirani_niz();
-    int len=(sizeof(nizakpod)/sizeof(*nizakpod));
-    cout << "print" << len<<endl;
-    for(int i=0;i<len;i++){
-        cout<<"Alocirani niz je "<<nizakpod[i]<<endl;
+    free(nizbrdo);
+
+    //peti zadatak!
+    int referenca=0;
+    int* nizakpod=alocirani_niz(referenca);
+    //int len=(sizeof(nizakpod)/sizeof(nizakpod));
+    
+    cout << referenca<<endl;
+    for(int z=0;z<referenca;z++){
+        cout<<"Alocirani niz je "<<nizakpod[z]<<endl;
     }
     free(nizakpod);
 }
