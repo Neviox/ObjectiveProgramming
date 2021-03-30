4419fc7f5570316496fac420849ccfaabdb15610
Tin Martinic <neviox@users.noreply.github.com>
2021-03-30 20:05:51 +0200

    .

--- vjezba 4/zad2.cpp
+++ vjezba 4/zad2.cpp
@@ -1,41 +1,79 @@
 #include <iostream>
 #include "zad2.hpp"
 #include <string>
+#include <cmath>
 using namespace std;
 
 Board::Board(){
-    a= new int;
-    b=new int;
+    
+    matrix = new char*[a];
     cout<<"Unesi sirinu i visina ploce"<<endl;
-    cin>>*a>>*b;
+    cin>>a>>b;
     cout<<"Stvaranje ruba s znakom "<<rub<<endl;
+    for(int i = 0; i < a; i++){
+        matrix[i] = new char[b];
     }
+}
 Board::Board(Board &ploca){ //---------------------Copy constructor---------------//
     cout<<"Kopiranje klase s znakom"<<rub<<endl;
     a=ploca.a;
     b=ploca.b;
     }
-Board::Board(int* a,int* b){ //------------------Izrada Ploce-----------------//
-     for (int i = 1; i <= *a; i++) 
+Board::Board(int z,int p){ //------------------Izrada Ploce-----------------//
+    cout<<"Constructor izrade ploce"<<endl;
+    a=z;
+    b=p;
+    matrix = new char*[a];
+
+    // this.printBoard();
+     for(int i = 0; i < a; i++){
+        matrix[i] = new char[b];
+    }
+     for (int i = 1; i <= a; i++) 
     { 
-        for (int j = 1; j <= *b; j++) 
+        for (int j = 1; j <= b; j++) 
         { 
-            if ((i == 1 || i == *a) || (j == 1 || j == *b)) {cout << rub;}             
+            if ((i == 1 || i == a) || (j == 1 || j == b)) {cout << rub;}   
+            else if (matrix[i][j] == 1) {
+                cout << "x";
+            }          
             else{cout << " ";}
         } 
         cout << endl;
     } 
   
 }
-Board::~Board(){//-----------------deconstruktor----------//
-    cout<<"dekonstruktor"<<endl;
+Board::~Board(){//-----------------deconstructor----------//
+    cout<<"Board deconstructor"<<endl;
+    for(int i = 0; i < a; i++){
+        delete [] matrix[i];
+        delete [] matrix;
+    }
 }
 
-int main(){
-    Board p;
-    Board ploca1(p);
-    Board ploca2(ploca1.a,ploca1.b);
+ void Board::draw_char(Point p,char ch){
+    int xx=round(p.x);
+    int yy=round(p.y);
+    matrix[xx][yy] == 1;
+    // print_board()
     
+}
+Point::Point(double ar1,double ar2){
+    x=ar1;
+    y=ar2;
+    cout<<"Point constructor"<<endl;
+    
+}
+
+Point::~Point(){
+    cout<<"Point decostructor"<<endl;
+}
+int main(){
+    //  Board ploca;
+    //  Board ploca2(ploca.a,ploca.b);
+    Point p1(2,2), p2(8, 8), p3(2,8),p4(16, 8);
+    Board b(20, 10);
+    b.draw_char(p2,'x');
 
 
 return 0;
--- vjezba 4/zad2.hpp
+++ vjezba 4/zad2.hpp
@@ -1,18 +1,24 @@
+struct Point{
+  public:
+    double x,y;
+    const char ch='x';
+    Point(double x,double y);
+    void draw_up_line(Point p,Point p1);
+    void draw_line(Point p,Point p1);
+    void display();
+    ~Point();
+};
 class Board{
   public:
-    int* a;
-    int* b;
+    int a;
+    int b;
     const char rub='!';
+    char** matrix;
+    void draw_char(Point p,char ch);
     Board();
     Board(Board &b);
-    Board(int* a,int* b);
+    Board(int a,int b);
     ~Board();
-    
-};
-struct Point{
-    double x,y;
-    void draw_char(int p,char ch);
-    void draw_up_line(int p,char ch);
-    void draw_line(int p1,int p2,char ch);
-    void display(Board b,int p1,int p2,char ch);
-};
+};    
+
+
