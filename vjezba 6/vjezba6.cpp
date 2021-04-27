1de6819a8adcb7ac2fcaa12151c10bbd26a317c4
Tin Martinic <neviox@users.noreply.github.com>
2021-04-27 20:24:47 +0200

    .

---
+++ vjezba 6/vjezba6.cpp
@@ -0,0 +1,22 @@
+#include "vjezba6.hpp"
+#include <iostream>
+using namespace std;
+
+Zivotinja::~Zivotinja(){
+
+}
+
+int main(){
+    Zohar tukac;
+    tukac.print();
+    Tarantula tarantino;
+    tarantino.print();
+    Orao kopao;
+    kopao.print();
+    Brojac c;
+    c.set_zbroj(tukac);
+    c.set_zbroj(tarantino);
+    c.set_zbroj(kopao);
+    cout<<c.get_zbroj();
+    return 0;
+}
---
+++ vjezba 6/vjezba6.hpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Zivotinja{
+        unsigned noge;
+        string nova_zivotinja="dodan:";
+    public:
+        virtual int broj_nogu()=0;
+        virtual string vrsta(){return nova_zivotinja;}
+        virtual void print(){cout<<vrsta()<<"\n";}
+        virtual ~Zivotinja();
+};
+class Kukac : public Zivotinja{
+    public:
+         int broj_nogu()=0;
+
+};
+class Pauk : public Zivotinja{
+    public:
+         int broj_nogu()=0;
+};
+class Ptica :public Zivotinja{
+    public:
+         int broj_nogu()=0;
+};
+class Zohar : public Kukac{
+    string ime="Zohar";
+    int noge=8;
+    public:
+         int broj_nogu(){return noge;}
+         string vrsta(){return ime;}
+        void print(){cout<<vrsta()<<"\n";}
+};
+class Tarantula : public Pauk{
+        string ime="Tarantula";
+        int noge=4;
+    public:
+         int broj_nogu(){return noge;};
+         string vrsta(){return ime;}
+         void print(){cout<<vrsta()<<"\n";}
+};
+class Orao : public Ptica{
+    string ime="Orao";
+    int noge=2;
+    public:
+         int broj_nogu(){return noge;}
+         string vrsta(){return ime;}
+         void print(){cout<<vrsta()<<"\n";}
+};
+
+class Brojac{
+    unsigned zbroj;
+    public:
+        void reset(){zbroj=0;}
+        void set_zbroj(Zivotinja &ziv){zbroj+=ziv.broj_nogu();}
+        int get_zbroj(){return zbroj;}
+        
+};
