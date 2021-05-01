0b67945d4086aa39a50247fb4e7ed4d35f518bab
Tin Martinic <neviox@users.noreply.github.com>
2021-05-01 14:39:25 +0200

    .

---
+++ vjezba 7/vjezba7.cpp
@@ -0,0 +1,17 @@
+#include "vjezba7.hpp"
+#include <iostream>
+using namespace std;
+
+
+ 
+int main () {
+
+Counter c;
+Vehicle* v[] = {new Catamaran(30), new Ferry(10, 5, 3), new Seaplane(15)};
+size_t sz =sizeof v /sizeof v[0];
+    for(unsigned i = 0; i < sz; ++i){c.ukupno(v[i]);}
+    cout << "ukupno " << c.get_zbroj() << " putnika" << endl;
+    for(unsigned i = 0; i < sz; ++i){delete v[i];}
+
+return 0;
+}
---
+++ vjezba 7/vjezba7.hpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+class Vehicle{                          //------------------------bazna klasa----------------------------//
+    unsigned num_passangers;
+    public:
+    virtual string get_type()=0;
+    virtual int get_num_passangers(){return num_passangers;}
+    virtual ~Vehicle(){
+    }
+};
+
+class Watercraft:public Vehicle{        //---------------------vodeni tip vozila----------------------------//
+    string type="water";
+    public:
+    string get_type(){return type;}
+
+};
+
+class Aircraft:public Vehicle{          //------------------------zracni tip vozila----------------------//
+    string type="air";
+    public:
+    string get_type(){return type;}
+
+};
+
+class Seaplane:public Vehicle{          //------------------------------hidroavion-------------------------//
+    string type="air-water";
+    unsigned num_passagers;
+    public:
+    Seaplane(int n){
+        num_passagers=n;
+        cout<<get_type()<< " putnika "<<get_num_passangers()<<endl;
+    }
+    string get_type(){return type;}
+    int get_num_passangers(){return num_passagers;}
+    ~Seaplane(){
+    }
+
+};
+
+class Catamaran:public Watercraft{          //-----------------------------katamaran(vjerojatno za hvar,na hvaru je ludnica)--------------------//
+    string type="water";
+    unsigned num_passagers;
+    public:
+    Catamaran(int n){
+        num_passagers=n;
+        cout<<get_type()<< " putnika "<<get_num_passangers()<<endl;
+    }
+    string get_type(){return type;}
+    int get_num_passangers(){return num_passagers;}
+    ~Catamaran(){
+    }
+
+};
+class Ferry:public Vehicle{                 //------------------------------------trajekt(zimska linija ocito)--------------------------------//
+    string type="water";
+    unsigned num_passagers;
+    unsigned setaci;
+    unsigned bicikle;
+    unsigned automobil;
+    public:
+    Ferry(int n,int k,int z){
+        setaci=n;
+        bicikle=k;
+        automobil=z*5;
+        num_passagers=setaci+bicikle+automobil;
+        cout<<get_type()<< " putnika "<<get_num_passangers()<<" od toga u automobilu "<<get_automobil()<< " i na bicikli "<<get_bicikle()<<endl;
+    }
+    string get_type(){return type;}
+    int get_num_passangers(){return num_passagers;}
+    int get_bicikle(){return bicikle;}
+    int get_automobil(){return automobil;}
+    ~Ferry(){
+    }
+
+};
+
+class Counter{                                    //----------------------------------broji glave---------------------//
+    protected:
+    int zbroj;
+    public:
+    Counter(){
+        
+    }
+    void ukupno(Vehicle* v){
+        zbroj+=v->get_num_passangers();
+    }
+    int get_zbroj(){return zbroj;}
+    ~Counter(){
+    
+    }
+};
